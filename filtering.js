c07176651847b048158d469dc20fde013434ae76
wongnick <nickyrwong@gmail.com>
2017-04-13 14:08:56 -0700

    adding list grid toggle feature..

--- filtering.js
+++ filtering.js
@@ -1,24 +1,48 @@
 
 
+// when dom is loaded
 $(document).ready(function(){
 	
+	// When a category is clicked
 	$('.cat_item').bind("click", function(){
+		
+		// Find which category that was clicked
 		var category = '.' + $(this).attr('id');
-		if($(this).attr('class') != "cat_item active"){
-			$('#all, #animals, #food, #scenery').removeClass('active');
-			$(this).addClass('active');
+		
+		// If the category clicked is not active, set it to active 
+		// and show the corresponding filtered blocks
+		if(!$(this).hasClass("active")){
+			$('.cat_item').removeClass("active");
+			$(this).addClass("active");
 		}
+
+		// Show all blocks if the 'all' tab was clicked
+		// Otherwise, show only the selected category blocks
 		if(category == '.all'){
-			$('.animals, .food, .scenery').addClass('hide');
+			$('.block_container img').addClass("hidden");
 			setTimeout(function(){
-				$('.animals, .food, .scenery').removeClass('hide');
+				$('.block_container img').removeClass("hidden");
 			}, 300);
 		}else{
-			$('.animals, .food, .scenery').addClass('hide');
+			$('.block_container img').addClass("hidden");
 			setTimeout(function(){
-				$(category).removeClass('hide');
+				$(category).removeClass("hidden");
 			}, 300);
+		}
 
+	});	
+
+	//Toggle between grid and list format
+	$('.toggle').bind("click", function(){
+		if($(this).attr('id') == "list"){
+			$('.toggle').removeClass("active");
+			$(this).addClass("active");
+			$(".block_container [id*='grid']").attr('id', 'list');
+		}else{
+			$('.toggle').removeClass("active");
+			$(this).addClass("active");
+			$(".block_container [id*='list']").attr('id', 'grid');
 		}
 	});
+	
 });
